aadb6b4d6a96812239f3ef072adcf78987243a62
BhaskarAcharjee <bhaskaracharjee2002@gmail.com>
2024-06-18 04:08:03 +0530

    Login & Signup form improved

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -1,23 +1,40 @@
-import "./userInfo.css"
+import { useState } from "react";
+import "./userInfo.css";
 import { useUserStore } from "../../../lib/userStore";
+import { auth } from "../../../lib/firebase";
 
 const Userinfo = () => {
-
   const { currentUser } = useUserStore();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const logoutUser = () => {
+    auth.signOut();
+  };
 
   return (
-    <div className='userInfo'>
+    <div className="userInfo">
       <div className="user">
         <img src={currentUser.avatar || "./avatar.png"} alt="" />
         <h2>{currentUser.username}</h2>
       </div>
       <div className="icons">
-        <img src="./more.png" alt="" />
+        <img src="./more.png" alt="" onClick={toggleDropdown} />
         <img src="./video.png" alt="" />
         <img src="./edit.png" alt="" />
+        {dropdownOpen && (
+          <div className="dropdown">
+            <div className="dropdownItem" onClick={logoutUser}>
+            <img src="./logout.png" alt="" />
+            </div>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Userinfo
+export default Userinfo;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -16,6 +16,7 @@ const Login = () => {
   });
 
   const [loading, setLoading] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
 
   const handleAvatar = (e) => {
     if (e.target.files[0]) {
@@ -69,6 +70,7 @@ const Login = () => {
       toast.error(err.message);
     } finally {
       setLoading(false);
+      setIsLogin(true);
     }
   };
 
@@ -92,32 +94,49 @@ const Login = () => {
   return (
     <div className="login">
       <div className="item">
-        <h2>Welcome back,</h2>
-        <form onSubmit={handleLogin}>
-          <input type="text" placeholder="Email" name="email" />
-          <input type="password" placeholder="Password" name="password" />
-          <button disabled={loading}>{loading ? "Loading" : "Sign In"}</button>
-        </form>
-      </div>
-      <div className="separator"></div>
-      <div className="item">
-        <h2>Create an Account</h2>
-        <form onSubmit={handleRegister}>
-          <label htmlFor="file">
-            <img src={avatar.url || "./avatar.png"} alt="" />
-            Upload an image
-          </label>
-          <input
-            type="file"
-            id="file"
-            style={{ display: "none" }}
-            onChange={handleAvatar}
-          />
-          <input type="text" placeholder="Username" name="username" />
-          <input type="text" placeholder="Email" name="email" />
-          <input type="password" placeholder="Password" name="password" />
-          <button disabled={loading}>{loading ? "Loading" : "Sign Up"}</button>
-        </form>
+        <h2>Welcome to ChitChat!</h2>
+        {isLogin ? (
+          <>
+            <h3>Welcome back,</h3>
+            <form onSubmit={handleLogin}>
+              <input type="text" placeholder="Email" name="email" />
+              <input type="password" placeholder="Password" name="password" />
+              <button disabled={loading}>{loading ? "Signing in.." : "Sign In"}</button>
+            </form>
+            <p>
+              New to ChitChat?{" "}
+              <span className="link" onClick={() => setIsLogin(false)}>
+                Create an Account
+              </span>
+            </p>
+          </>
+        ) : (
+          <>
+            <h3>Create an Account</h3>
+            <form onSubmit={handleRegister}>
+              <label htmlFor="file">
+                <img src={avatar.url || "./avatar.png"} alt="" />
+                Upload an image
+              </label>
+              <input
+                type="file"
+                id="file"
+                style={{ display: "none" }}
+                onChange={handleAvatar}
+              />
+              <input type="text" placeholder="Username" name="username" />
+              <input type="text" placeholder="Email" name="email" />
+              <input type="password" placeholder="Password" name="password" />
+              <button disabled={loading}>{loading ? "Signing up.." : "Sign Up"}</button>
+            </form>
+            <p>
+              Already have an account?{" "}
+              <span className="link" onClick={() => setIsLogin(true)}>
+                Sign In
+              </span>
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
